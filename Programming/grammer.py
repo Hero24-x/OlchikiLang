0176ee90bc81d7755c8738b9d5c0997b61959591
Shibnath Hansda <196810131+Hero24-x@users.noreply.github.com>
2026-05-21 12:34:23 +0530

    Create grammer.py

---
+++ Programming/grammer.py
@@ -0,0 +1,23 @@
+KEYWORDS = {
+    "ᱪᱟᱯᱟ",   # print
+    "ᱥᱮᱴ",    # set
+    "ᱯᱩᱱ",    # function
+    "ᱫᱟᱨ",    # return
+    "ᱡᱩᱫᱤ",   # if
+    "ᱮᱞᱥᱮ",   # else
+    "ᱫᱚᱦᱚᱨ",  # loop
+    "ᱢᱟᱨᱟ",   # var
+    "ᱢᱟᱨᱟ ᱪᱟᱯᱟ",  # var print
+    "ᱢᱟᱨᱟ ᱥᱮᱴ",   # var set
+    "ᱢᱟᱨᱟ ᱯᱚᱱ",   # var function
+    "ᱚ ᱫᱚ ᱨ",   # var return
+    "ᱢᱟᱨᱟ ᱡᱩᱫᱤ",   # var if
+    "ᱢᱟᱨᱟ ᱮᱞᱥᱮ",   # var else
+    "ᱢᱟᱨᱟ ᱫᱚᱦᱚᱨ",  # var loop
+    "ᱢᱟᱨᱟ ᱢᱟᱨᱟ",   # var var
+    "ᱢᱟᱨᱟ ᱢᱟᱨᱟ ᱪᱟᱯᱟ",  # var var print
+    "ᱢᱟᱨᱟ ᱢᱟᱨᱟ ᱥᱮᱴ",   # var var set
+    "ᱢᱟᱨᱟ ᱢᱟᱨᱟ ᱯᱩᱱ",   # var var function
+    "ᱢᱟᱨᱟ ᱢᱟᱨᱟ ᱫᱟᱨ",   # var var return
+    "ᱢᱟᱨᱟ ᱢᱟᱨᱟ ᱡᱩᱫᱤ",   # var var if
+}
